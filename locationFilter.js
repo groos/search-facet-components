d0f1163df6a4290be75f1153018fab668a31f1fc
Nick Groos <NGroos@rightpoint.com>
2016-11-18 14:45:51 -0600

    Am fetching suggested locations and lat/long from google api

--- locationFilter.js
+++ locationFilter.js
@@ -6,6 +6,24 @@ function LocationFilter(element, options, bindings){
     this.$element = Coveo.$(element);
 
     this.suggestedLocations = [];
+
+
+    var wrapper = Coveo.$('<div />', {"id" : LocationFilter.ID,
+                                      "class" : this.wrapperClass,
+                                      "css" : {"width" : "200px"}});
+
+    var inputBox = Coveo.$('<input />', {"id" : "location-filter-input", 
+                          "type" : "text",
+                          "placeholder" : this.options.title});
+
+    //inputBox.keyup(this.getSuggestedLocations.bind(this));
+    inputBox.appendTo(wrapper);
+    this.buildSuggestedList(wrapper);
+    wrapper.appendTo(this.$element);
+
+    this.autocomplete = new google.maps.places.Autocomplete((this.$element.find("#location-filter-input").get(0)),
+                                                            {types: ['geocode']});          
+    this.autocomplete.addListener('place_changed', this.sortByLocation.bind(this));                                                  
 };
 
 LocationFilter.ID = 'LocationFilter';
@@ -13,24 +31,25 @@ LocationFilter.options = {
     title: Coveo.ComponentOptions.buildStringOption()
 }
 
+LocationFilter.prototype.sortByLocation = function(){
+    var place = this.autocomplete.getPlace();
+
+    var latitude = place.geometry.location.lat();
+    var longitude = place.geometry.location.lng();
+};
+
 LocationFilter.prototype.buildComponent = function(groupByResults){
-    this.$element.find(this.wrapperClass).unbind().remove();
+    //this.$element.find(this.wrapperClass).unbind().remove();
 
-    var wrapper = Coveo.$('<div />', {"class" : this.wrapperClass,
-                                      "css" : {"width" : "200px"}});
-    var inputBox = Coveo.$('<input />', {"class" : "location-filter-input", 
-                          "type" : "text",
-                          "placeholder" : this.options.title});
-
-    inputBox.keyup(this.getSuggestedLocations.bind(this));
-    inputBox.appendTo(wrapper);
-
-    this.buildSuggestedList(wrapper);
-
-    wrapper.appendTo(this.$element);
+    
+    
+    
+    
 };
 
 LocationFilter.prototype.buildSuggestedList = function(wrapper){
+    this.$element.find("." + "location-wrapper").unbind().remove();
+
     this.suggestedLocations.forEach(function(location){
         var locationWrapper = Coveo.$('<div />', {"class" : "location-wrapper"});
         // TODO - sniff these objects for the field name
@@ -45,6 +64,9 @@ LocationFilter.prototype.getSuggestedLocations = function(e){
     var searchTerms = e.target.value;
 
     // Go out and get the suggested list, given these search terms
+    var places = this.autocomplete.getPlace();
+
+    var x = 1;
 };
 
 Coveo.CoveoJQuery.registerAutoCreateComponent(LocationFilter);
