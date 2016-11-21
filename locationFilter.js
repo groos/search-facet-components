1dce2686d3d5963c2ad5a25d6ab8fe543301e8e6
Nick Groos <NGroos@rightpoint.com>
2016-11-21 13:32:40 -0600

    very rough location filter prototype working

--- locationFilter.js
+++ locationFilter.js
@@ -1,12 +1,18 @@
-__extends(LocationFilter, FacetBase);
+__extends(LocationFilter, _super);
 function LocationFilter(element, options, bindings){
-    FacetBase.call(this, element, options, bindings, LocationFilter.ID);
-    this.options = Coveo.ComponentOptions.initComponentOptions(element, RadioButtonFacet, options);
+    _super.call(this, element, LocationFilter.ID, bindings);
+    this.options = Coveo.ComponentOptions.initComponentOptions(element, LocationFilter, options);
+
+    this.stateName = Coveo.QueryStateModel.getFacetId(this.options.field);
+
+    this.queryStateModel.registerNewAttribute(this.stateName, []);
+
     this.wrapperClass = 'location-filter-wrapper';
     this.$element = Coveo.$(element);
 
-    this.suggestedLocations = [];
-
+    this.bind.onRoot(Coveo.QueryEvents.buildingQuery, this.handleBuildingQuery);
+    this.bind.onRoot(Coveo.QueryEvents.doneBuildingQuery, this.handleDoneBuildingQuery);
+    this.bind.onRoot(Coveo.QueryEvents.deferredQuerySuccess, this.handleQuerySuccess);
 
     var wrapper = Coveo.$('<div />', {"id" : LocationFilter.ID,
                                       "class" : this.wrapperClass,
@@ -16,57 +22,50 @@ function LocationFilter(element, options, bindings){
                           "type" : "text",
                           "placeholder" : this.options.title});
 
-    //inputBox.keyup(this.getSuggestedLocations.bind(this));
     inputBox.appendTo(wrapper);
-    this.buildSuggestedList(wrapper);
     wrapper.appendTo(this.$element);
 
     this.autocomplete = new google.maps.places.Autocomplete((this.$element.find("#location-filter-input").get(0)),
                                                             {types: ['geocode']});          
-    this.autocomplete.addListener('place_changed', this.sortByLocation.bind(this));                                                  
+    this.autocomplete.addListener('place_changed', this.locationPicked.bind(this));                                                  
 };
 
 LocationFilter.ID = 'LocationFilter';
 LocationFilter.options = {
+    field: Coveo.ComponentOptions.buildStringOption(),
     title: Coveo.ComponentOptions.buildStringOption()
 }
 
-LocationFilter.prototype.sortByLocation = function(){
-    var place = this.autocomplete.getPlace();
-
-    var latitude = place.geometry.location.lat();
-    var longitude = place.geometry.location.lng();
+LocationFilter.prototype.buildQueryFunction = function(){
+    if (this.latitude && this.longitude){
+        // return {"function":"dist(@latitude, @longitude, " + this.latitude + ", " + this.longitude + ")", "fieldName":"distance"};
+        return {"function":"dist(@flatitude22573, @flongitude22573, " + this.latitude + ", " + this.longitude + ")", "fieldName":"distance"};
+    }
 };
 
-LocationFilter.prototype.buildComponent = function(groupByResults){
-    //this.$element.find(this.wrapperClass).unbind().remove();
-
-    
-    
-    
-    
+LocationFilter.prototype.handleBuildingQuery = function(e, data) {
+    var queryFunction = this.buildQueryFunction();
+    if (queryFunction){
+        data.queryBuilder.queryFunctions.push(queryFunction);
+        data.queryBuilder.sortCriteria = "@distance ascending";
+    }
 };
 
-LocationFilter.prototype.buildSuggestedList = function(wrapper){
-    this.$element.find("." + "location-wrapper").unbind().remove();
+LocationFilter.prototype.handleDoneBuildingQuery = function(e, args) {
+    // now that the query is built, add the group-by request
 
-    this.suggestedLocations.forEach(function(location){
-        var locationWrapper = Coveo.$('<div />', {"class" : "location-wrapper"});
-        // TODO - sniff these objects for the field name
-        Coveo.$('<p />', {"text" : "location name"}).appendTo(locationWrapper);
-        locationWrapper.appendTo(wrapper);
-    })
 };
 
-LocationFilter.prototype.getSuggestedLocations = function(e){
-    this.suggestedLocations = [];
-
-    var searchTerms = e.target.value;
-
-    // Go out and get the suggested list, given these search terms
-    var places = this.autocomplete.getPlace();
-
+LocationFilter.prototype.handleQuerySuccess = function(e, data) {
     var x = 1;
+};
+
+LocationFilter.prototype.locationPicked = function(){
+    var place = this.autocomplete.getPlace();
+    this.latitude = place.geometry.location.lat();
+    this.longitude = place.geometry.location.lng();
+
+    this.queryStateModel.set(this.stateName, [true]);
 };
 
 Coveo.CoveoJQuery.registerAutoCreateComponent(LocationFilter);
