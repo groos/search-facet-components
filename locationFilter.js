7063e9a2c8e03a595807691bf86e3e6c6e895d24
Nick Groos <NGroos@rightpoint.com>
2016-11-23 10:10:56 -0600

    added a button to clear location filter

--- locationFilter.js
+++ locationFilter.js
@@ -10,24 +10,17 @@ function LocationFilter(element, options, bindings){
     this.$element = Coveo.$(element);
 
     this.bind.onRoot(Coveo.QueryEvents.buildingQuery, this.handleBuildingQuery);
-    this.bind.onRoot(Coveo.QueryEvents.doneBuildingQuery, this.handleDoneBuildingQuery);
-    this.bind.onRoot(Coveo.QueryEvents.deferredQuerySuccess, this.handleQuerySuccess);
 
-    var wrapper = Coveo.$('<div />', {"id" : LocationFilter.ID,
-                                      "class" : this.wrapperClass,
-                                      "css" : {"width" : "200px"}});
-
-    var inputBox = Coveo.$('<input />', {"id" : "location-filter-input", 
-                          "type" : "text",
-                          "placeholder" : this.options.title});
-
-    inputBox.appendTo(wrapper);
-    wrapper.appendTo(this.$element);
-
-    this.autocomplete = new google.maps.places.Autocomplete((this.$element.find("#location-filter-input").get(0)),
-                                                            {types: ['geocode']});          
-    this.autocomplete.addListener('place_changed', this.locationPicked.bind(this));                                                  
+    this.buildComponent();                                                 
 };
+
+
+/*
+
+    To Add:
+        - Add breadcrumb support?
+
+*/
 
 LocationFilter.ID = 'LocationFilter';
 LocationFilter.options = {
@@ -36,6 +29,46 @@ LocationFilter.options = {
     longitude: Coveo.ComponentOptions.buildStringOption(),
     title: Coveo.ComponentOptions.buildStringOption()
 }
+
+LocationFilter.prototype.buildComponent = function() {
+    var wrapper = Coveo.$('<div />', {"id" : LocationFilter.ID,
+                                      "class" : this.wrapperClass,
+                                      "css" : {"width" : "200px", "display" : "inline"}});
+
+    var inputBox = Coveo.$('<input />', {"id" : "location-filter-input", 
+                          "type" : "text",
+                          "placeholder" : this.options.title});
+
+    inputBox.appendTo(wrapper);
+
+    var clearLocationWrapper = Coveo.$('<div />', {"class" : "clear-location-wrapper",
+                                "css" : {"background-color" : "red", 
+                               "display" : "inline-block",
+                               "text-align" : "center",
+                               "margin-left" : "10px",
+                               "width" : "50px"}});
+
+    Coveo.$('<p />', {"id" : "clear-location-button", 
+                      "text" : "X"
+                      })
+                      .appendTo(clearLocationWrapper);
+
+    clearLocationWrapper.click(this.clearLocationFilter.bind(this));
+
+    clearLocationWrapper.appendTo(wrapper);
+    wrapper.appendTo(this.$element);
+
+    // Google Autocomplete bindings
+    this.autocomplete = new google.maps.places.Autocomplete((this.$element.find("#location-filter-input").get(0)),
+                                                            {types: ['geocode']});          
+    this.autocomplete.addListener('place_changed', this.locationPicked.bind(this)); 
+};
+
+LocationFilter.prototype.clearLocationFilter = function(){
+    this.$element.find('.' + this.wrapperClass).unbind().remove();
+    this.buildComponent();
+    this.locationPicked();
+};
 
 LocationFilter.prototype.buildQueryFunction = function(){
     if (this.latitude && this.longitude){
@@ -51,18 +84,16 @@ LocationFilter.prototype.handleBuildingQuery = function(e, data) {
     }
 };
 
-LocationFilter.prototype.handleDoneBuildingQuery = function(e, args) {
-    
-};
-
-LocationFilter.prototype.handleQuerySuccess = function(e, data) {
-    
-};
-
 LocationFilter.prototype.locationPicked = function(){
     var place = this.autocomplete.getPlace();
-    this.latitude = place.geometry.location.lat();
-    this.longitude = place.geometry.location.lng();
+
+    if (place){
+        this.latitude = place.geometry.location.lat();
+        this.longitude = place.geometry.location.lng();
+    } else {
+        this.latitude = null;
+        this.longitude = null;
+    }
 
     this.queryController.deferExecuteQuery();
 };
